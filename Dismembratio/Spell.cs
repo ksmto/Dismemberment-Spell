41c9814271e6e47532f15118278461af660f5bf8
Kishimoto <wyatt.woodley@outlook.com>
2023-03-04 00:14:06 -0500

    Initial commit

---
+++ Dismembratio/Spell.cs
@@ -0,0 +1,12 @@
+﻿using ThunderRoad;
+using Extensions;
+
+namespace Dismembratio;
+public class Spell : SpellCastCharge {
+    public override void UpdateCaster() {
+        base.UpdateCaster();
+        if (!spellCaster.isFiring) return;
+        var creature = Player.currentCreature.GetClosestCreature();
+        creature.SliceAllParts();
+    }
+}
